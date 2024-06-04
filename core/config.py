d88bbc65dba320089701b77b6c400a11accaadd6
Lynn <vanderhorstlynn@gmail.com>
2024-06-04 12:29:13 +0200

    Add waypoint generation

--- core/config.py
+++ core/config.py
@@ -2,3 +2,9 @@ class Config:
     width = 1280
     height = 720
 
+    min_node_v = 50
+    max_node_v = 100
+    node_v_d = 20
+
+    granularity = 5  # Decimals
+
--- core/node_factory.py
+++ core/node_factory.py
@@ -11,41 +11,70 @@ and the Node objects themselves
 """
 
 
-def generate_waypoint_array(start, length, p_factor=1, s_factor=1, seed=None):
-    # Generate first waypoint to determine initial direction
-    w_x = start.x + (s_factor * random.randint(start.x, Config.width - start.x))
-    w_y = start.x + (s_factor * random.randint(start.y, Config.height - start.y))
+def point_bounce(position):
+    if position.x < 0 or position.x > Config.width:
+        x_remainder = position.x % Config.width
+        if int(position.x / Config.width) % 2 == 0:
+            position.x = Config.width - x_remainder
+        else:
+            position.x = x_remainder
 
-    w_last = Coordinate(w_x, w_y)
-    waypoints = [w_last]
+    if position.y < 0 or position.y > Config.height:
+        y_remainder = position.y % Config.height
+        if int(position.y / Config.height) % 2 == 0:
+            position.y = Config.height - y_remainder
+        else:
+            position.y = y_remainder
 
-    for i in range(length - 1):
-        dx = start.x - w_last.x
-        dy = start.y - w_last.y
-        w_angle = math.atan2(dx, dy)
-        angle_delta = p_factor * random.uniform(0, 2*math.pi)
+    return position
 
-        w_x = start.x + (s_factor * random.randint(start.x))
+
+def generate_waypoint_array(start, length, h_factor=1, v_factor=1):
+    if length < 0:
+        Exception('Length cannot be smaller than 0')
+    if length == 0:
+        return []
+
+    waypoints = [start]
+    w_h = random.uniform(0, 2 * math.pi)  # Random heading (rad)
+    w_v = random.uniform(Config.min_node_v, Config.max_node_v)  # Random velocity
+
+    for i in range(length):
+        w_x = round(waypoints[-1].x + w_v * math.cos(w_h), Config.granularity)  # Determine waypoint x
+        w_y = round(waypoints[-1].y + w_v * math.sin(w_h), Config.granularity)  # Determine waypoint y
+
+        waypoints.append(point_bounce(Coordinate(w_x, w_y)))  # Add waypoint to array
+
+        w_h = \
+            (math.atan2(waypoints[-2].x - waypoints[-1].x, waypoints[-2].y - waypoints[-1].y)
+             + h_factor * random.uniform(0, 2 * math.pi)) % 2 * math.pi
+
+        w_v = min(max(w_v + v_factor * random.uniform(-0.5 * Config.node_v_d, 0.5 * Config.node_v_d), 0), Config.min_node_v)
+
+    # w_heading = math.atan2(dx, dy)
+
     return waypoints
 
 
-def generate_coordinate_array(length, max_x, max_y, min_x=0, min_y=0, seed=None):
-    return [(generate_coordinate(max_x, max_y, min_x, min_y, seed=seed)) for _ in range(length)]
+def generate_coordinate_array(length, max_x, max_y, min_x=0, min_y=0):
+    return [(generate_coordinate(max_x, max_y, min_x, min_y)) for _ in range(length)]
 
 
-def generate_coordinate(max_x, max_y, min_x=0, min_y=0, seed=None):
-    random.seed(seed)
+def generate_coordinate(max_x, max_y, min_x=0, min_y=0):
     return random.randint(min_x, max_x), random.randint(min_y, max_y)
 
 
-def generate_nodes(count, max_x, max_y, min_x=0, min_y=0, seed=None):
+def generate_nodes(count, max_x, max_y, min_x=0, min_y=0):
     return [
         Node(x, y, generate_waypoint_array()) for x, y in
-        generate_coordinate_array(count, max_x, max_y, min_x, min_y, seed=seed)
+        generate_coordinate_array(count, max_x, max_y, min_x, min_y)
     ]
 
 
 if __name__ == "__main__":
-    print(generate_waypoint_array(Coordinate(0, 0), 1, 1, 1 ))
-    print(generate_coordinate_array(10, 500, 500, seed=8908342))
-    print(generate_coordinate_array(10, 500, 500, seed=8908341))
+    random.seed(8908342)
+    gen = generate_waypoint_array(Coordinate(50, 50), 100)
+    for coord in gen:
+        print(f"x: {coord.x}, y: {coord.y}" )
+    print(generate_coordinate_array(10, 500, 500))
+    print(generate_coordinate_array(10, 500, 500))
